7135d3cc6d101f6b5b7a3a510fc0fd4eb36e40bb
Mohammed Noori <kmanoorib@gmail.com>
2020-02-23 02:50:46 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,11 +1,17 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render json: @birds
+    # render json: @birds
+    # another way to selectively choose data
+    render json: birds, only: [:id, :name, :species]
   end
   
   def show
     bird = Bird.find_by(id: params[:id])
-    render json: {id: bird.id, name: bird.name, species: bird.species}
+    
+    # render json: {id: bird.id, name: bird.name, species: bird.species}
+    # render json can also be done like this
+    # render json: bird.slice(:id, :name, :species)
+    # render json can also be done using only: [:whatever]
   end
 end
